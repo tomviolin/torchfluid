d7147b2d61ed3835bb19f4821acf86fa8067d569
Tom Hansen <tomh@uwm.edu>
2023-03-12 18:57:36 -0500

    many tweaks

--- fluid.py
+++ fluid.py
@@ -28,10 +28,16 @@ def mp(pn,dflt=None):
 
 TRAINING=not ('-t' in sys.argv or '-tr' in sys.argv)
 RESUMING='-r' in sys.argv or '-tr' in sys.argv
+resumeskip=1
+if sys.argv[-1].isnumeric():
+    resumeskip=int(sys.argv[-1])
 BATCHSIZE=mp('BATCHSIZE',16)
 num_epochs = mp('NUM_EPOCHS',4000)
 num_meta_epochs = mp('NUM_META_EPOCS',1)
-ITERS=mp('ITERS',10)+1
+ITERS=mp('ITERS',0)
+ITERS += 0.05
+MP['ITERS'] = int(ITERS*100+0.5)/100
+#ITERS = int(ITERS)
 SCRAMBLE_SIZE=mp('SCRAMBLE_SIZE',420)
 import cv2
 os.environ['CUDA_LAUNCH_BLOCKING']='1'
@@ -54,9 +60,6 @@ from PIL import Image
 from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
 from scipy.signal import fftconvolve
-
-
-
 
 def datagenerator():
     fname = sorted(glob("../lbinfo-*.csv"))[-1]
@@ -112,7 +115,7 @@ def datagenerator():
         #print(f"flowpV.shape={flowpV.shape}")
         flowpV[flowv>0] = 0
 
-        if len(flowqueue) == ITERS:
+        if len(flowqueue) == int(ITERS):
 
             """
             yield (
@@ -137,9 +140,53 @@ def datagenerator():
             flowpV.copy(),
             ))
 
-k = datagenerator()
-for i in range(np.random.randint(0,num_epochs*3)):
-    next(k)
+#k0 = datagenerator()
+"""
+idx=0
+while True:
+    try:
+        ftrain,ftarg = next(k0)
+        np.save(f'npy{os.path.sep}tra{idx:07d}.npy',ftrain)
+        np.save(f'npy{os.path.sep}tar{idx:07d}.npy',ftarg)
+        print(".",end='',flush=True)
+        if idx % 80 == 0:
+            print(idx,flush=True)
+        idx += 1
+    except:
+        break
+"""
+
+npydata = []
+idx=0
+print("LOADING DATA")
+while True:
+    ftra=f'npy{os.path.sep}tra{idx:07d}.npy'
+    if os.path.exists(ftra):
+        npydata.append(np.load(ftra))
+        print(".",end='',flush=True)
+        idx += 1
+        if idx % 100 == 0:
+            print(idx,flush=True)
+    else:
+        break
+
+
+
+def npydatagenerator(start=0):
+    while True:
+        ridx = np.random.permutation(len(npydata))
+        if np.sum(np.abs(np.diff(ridx))<3) < 3:
+            break
+
+    idx = start
+    while True:
+        yield npydata[ridx[idx]],npydata[(ridx[idx]+int(ITERS+.05)) % len(npydata)]
+        idx += 1
+        idx %= len(npydata)
+
+np.random.seed()
+
+k=npydatagenerator(np.random.randint(0,num_epochs))
 
 def TensorFluidBatch(k,batchsize):
     fmtra = []
@@ -154,14 +201,14 @@ def TensorFluidBatch(k,batchsize):
 
     return fm_train2, fm_target2
 
+datadir = f"datadir{os.path.sep}{timestamp}"
+os.makedirs(datadir, exist_ok=True)
 if TRAINING:
-    datadir = f"datadir{os.path.sep}{timestamp}"
-    os.makedirs(datadir, exist_ok=True)
     print(f"TRAINING: datadir={datadir}")
 
 if (not TRAINING) or RESUMING:
     sep=os.path.sep
-    resdatadir= sep.join(sorted(glob(f"datadir{sep}*{sep}cnnstate.dat"))[-1].split(sep)[:-1])
+    resdatadir= sep.join(sorted(glob(f"datadir{sep}*{sep}cnnstate.dat"))[-resumeskip].split(sep)[:-1])
     print(f"Loading pre-trained weights from: datadir={resdatadir}")
 
 class EMLoss(nn.Module):
@@ -192,10 +239,70 @@ class NSLoss(nn.Module):
     def forward(self,y,x):
         return (torch.exp(((y[:,0:3,...]-x[:,0:3,...]).abs()).sum()/np.prod(y.shape[0:-2])/100000.0)-1.0)*50
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def same_padding(in_shape,kernel_size,strides_in):
+
+    in_height, in_width = in_shape
+    filter_height, filter_width = kernel_size
+    strides=(None,strides_in[0],strides_in[1])
+    out_height = np.ceil(float(in_height) / float(strides[1]))
+    out_width  = np.ceil(float(in_width) / float(strides[2]))
+
+#The total padding applied along the height and width is computed as:
+
+    if (in_height % strides[1] == 0):
+        pad_along_height = max(filter_height - strides[1], 0)
+    else:
+        pad_along_height = max(filter_height - (in_height % strides[1]), 0)
+        if (in_width % strides[2] == 0):
+            pad_along_width = max(filter_width - strides[2], 0)
+        else:
+            pad_along_width = max(filter_width - (in_width % strides[2]), 0)
+
+    print(pad_along_height, pad_along_width)
+                  
+    #Finally, the padding on the top, bottom, left and right are:
+
+    pad_top = pad_along_height // 2
+    pad_bottom = pad_along_height - pad_top
+    pad_left = pad_along_width // 2
+    pad_right = pad_along_width - pad_left
+
+    print(pad_left, pad_right, pad_top, pad_bottom)
+    return(pad_top,pad_left)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class ICNN(nn.Module):
     def conv(in_channels=3,out_channels=3,kernel_size=3,stride=1,padding=(4,4),leak=0.0,pool_size=1,dropout=0.0):
         print(f"in_channels:{in_channels},out_channels:{out_channels},kernel_size:{kernel_size},"+
                 f"stride:{stride},padding:{padding},leak:{leak},pool_size:{pool_size},dropout:{dropout}")
+        padding=(kernel_size//2,kernel_size//2)
         return nn.Sequential(
             nn.Conv2d(
                 in_channels=in_channels,
@@ -209,6 +316,8 @@ class ICNN(nn.Module):
             nn.Dropout(dropout),
             nn.BatchNorm2d(out_channels)
         )
+
+
 
 
     def __init__(self):
@@ -252,13 +361,18 @@ class ICNN(nn.Module):
 
 cnn = ICNN()
 #print(cnn)
-loss_func = nn.MSELoss()
 
+loss_func = nn.MSELoss()
+lossfuncdesc="MSELoss"
+if int(mp('ITERS')*10+0.5)//2 *2 == int(mp('ITERS')*10+0.5):
+    loss_func=nn.L1Loss()
+    lossfuncdesc="L1Loss"
+print(f"LOSS: {lossfuncdesc}")
 
 from torch import optim
-optlr = 0.001
+optlr = mp('INIT_LR',0.001)
 optimizer = optim.Adam(cnn.parameters(), lr = optlr)
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,'min',0.1,patience=5)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,'min',0.5,patience=5)
 
 from torch.autograd import Variable
 
@@ -271,13 +385,17 @@ def norm(x):
 
 def train(num_epochs, cnn, k):
     cnn.cuda()
-    bestloss=1000
+    bestloss =1000
+    beststd = 1000
     try:
         for me in range(num_meta_epochs):
             # Train the model
             total_step = 1 #traindata.shape[0] // BATCHSIZE
             #print(f"total_step = {total_step}")
-            #optimizer.zero_grad()
+            optimizer.zero_grad()
+            losses=[]
+            lossavgs=[]
+            lossstds=[]
             for epoch in range(num_epochs):
                 traindata,targetdata = TensorFluidBatch(k,BATCHSIZE)
                 traindata=traindata.cuda()
@@ -290,34 +408,49 @@ def train(num_epochs, cnn, k):
                 #plt.imsave(f"{datadir}/{epoch:07d}ty1.png",norm(targetdata[0,1,...].cpu().detach().numpy()))
                 #plt.imsave(f"{datadir}/{epoch:07d}ty2.png",norm(targetdata[0,2,...].cpu().detach().numpy()))
                 for i in range(0,total_step):
+                    print (f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{total_step}], tr/tg:{traindata[0,0,20,20]:+8f}/{targetdata[0,0,20,20]:+8f} ',end='',flush=True)
                     # gives batch data, normalize x when iterate train_loader
                     #b_x = Variable(images)   # batch x
                     #b_y = Variable(labels)   # batch y
                     #print(f"b_x.shape = {b_x.shape}")
-                    optimizer.zero_grad()           
+                    #if epoch % BATCHSIZE*4 == 0:
+                    optimizer.zero_grad()
+                    print("calc: ",flush=True,end='')
                     output = cnn(traindata)
-                    if ITERS > 1:
-                        nextput = (cnn((output.to(device))))
-                        output = nextput
-                    loss = loss_func(output,targetdata[:,0:4,...].to(device))
+                    if int(ITERS) > 1:
+                        for ii in range(int(ITERS)-1):
+                            nextput = (cnn(output.to(device)))
+                            output = nextput
+                    loss = loss_func(output,targetdata[:,0:4,...].to(device))**0.2
+                    print("eval, ",flush=True,end='')
+
                     #loss.requires_grad = True
-                    #print(f"traindata.shape={traindata.shape}, output.shape={output.shape}", end='')
-                    # clear gradients for this training step   
+                    #print(f"traindata.shape={traindata.shape},
+                    #output.shape={output.shape}", end='') # clear gradients for
+                    #this training step   
                     
                     # backpropagation, compute gradients 
+                    print("bprop, ",flush=True,end='')
                     loss.backward()
-                    # apply gradients             
+                    # apply gradients
+                    print("appgrads, ",flush=True,end='')
                     optimizer.step()
-                    
+                    print("done. ",flush=True,end='')
+
+                    losses.append(loss.item())
+                    lossavg=np.mean(losses[max(0,epoch-20):epoch+1])
+                    lossstd=np.std(losses[max(0,epoch-20):epoch+1])
+                    lossavgs.append(lossavg)
+                    lossstds.append(lossstd)
                     if (i+1) % 1 == 0:
                         print ('{},{},{},{},{:.20f}' 
                                .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()),file=open(f'{datadir}/report.txt','a'))
-                        print (f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{total_step}], tr/tg:{traindata[0,0,20,20]}/{targetdata[0,0,20,20]} log Loss: {np.log(loss.item()):.6f}')
+                        print (f'log {lossfuncdesc}: {np.log10(loss.item()):.4f}',flush=True)
                     pass
-                    if loss.item() < bestloss:
+                    if lossstd < beststd and epoch>=20:
                         torch.save(cnn.state_dict(), f"{datadir}{os.path.sep}cnnstate.dat")
                         print(f"saved state to {datadir}{os.path.sep}cnnstate.dat",flush=True)
-                        bestloss=loss.item()
+                        beststd=lossstd
                     #j=os.system("nvidia-smi -i 0 -q |grep 'Used GPU' | awk -F: '{ print $2 }' | awk '{ print $1 }'>/tmp/gpumem") 
                     #gpumem = int(open("/tmp/gpumem",'r').read())
                     #print(f"mem used={gpumem}")
@@ -336,7 +469,10 @@ def train(num_epochs, cnn, k):
 
 
 if (not TRAINING) or RESUMING:
-    cnn.load_state_dict(torch.load(f"{resdatadir}{os.path.sep}cnnstate.dat"))
+    try:
+        cnn.load_state_dict(torch.load(f"{resdatadir}{os.path.sep}cnnstate.dat"))
+    except RuntimeError as e:
+        pass
     cnn.cuda()
     print(f"loaded {resdatadir}{os.path.sep}cnnstate.dat")
 
@@ -344,14 +480,15 @@ cnn.cuda()
 cnn.train().cuda()
 #summary(cnn.cuda(), (1,1,175,256))
 open(f"{datadir}{os.path.sep}model_params.json","w").write(json.dumps(MP,indent=4))
+open(f"model_params.json","w").write(json.dumps(MP,indent=4))
 if TRAINING:
     print("training.")
     train(num_epochs, cnn, k)
-    torch.save(cnn.state_dict(), f"{datadir}{os.path.sep}cnnstate.dat")
-    print(f"saved state to {datadir}{os.path.sep}cnnstate.dat")
+    torch.save(cnn.state_dict(), f"{datadir}{os.path.sep}cnnfinalstate.dat")
+    print(f"saved state to {datadir}{os.path.sep}cnnfinalstate.dat")
 
 
-ktest = datagenerator()
+ktest = npydatagenerator()
 
 
 def test():
@@ -359,13 +496,12 @@ def test():
     # Test the model
     cnn.eval()
     cnn.cuda()
+    test_data,test_target = TensorFluidBatch(ktest,1)
     while(True):
         with torch.no_grad():
-            test_data,test_target = TensorFluid(ktest,1)
             test_data = test_data.cuda()
-            test_target = test_target.cuda()
-            output = idct_2d(cnn(dct_2d(test_data)))
-            vorticity = output[0,0,...]
+            output = cnn(test_data)
+            vorticity = test_data[0,0,...]
             vorticity = vorticity.cpu().detach().numpy()
             vort=np.zeros(vorticity.shape+(3,))
             vort[...,0] = np.abs(vorticity) * (vorticity>=0)
@@ -376,9 +512,15 @@ def test():
             cv2.imshow('test2',vort)
             ky = cv2.waitKey(5000)
             if ky in [3,27,81,113]: break
+            if ky in [ord('n'),ord("N")]:
+                test_data,test_target=TensorFluidBatch(ktest,1)
+            else:
+                test_data = output
         pass
     pass
 
+if not TRAINING:
+    test()
 
 
 """
